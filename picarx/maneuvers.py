58a84ee2cf5339a2e8445ea257b69c4aab81f93c
NathanJustus <nathantjustus@gmail.com>
2024-01-03 17:34:29 -0800

    Update maneuvers.py

--- picarx/maneuvers.py
+++ picarx/maneuvers.py
@@ -8,7 +8,7 @@ def parkRight(car):
 	time.sleep(2)
 	car.stop()
 
-	angleRange = [-30,25]
+	angleRange = [-30,26]
 	car.set_dir_servo_angle(angleRange[0])
 	time.sleep(0.5)
 
@@ -20,7 +20,27 @@ def parkRight(car):
 	car.stop()
 	car.set_dir_servo_angle(0)
 
+def parkLeft(car):
+	car.stop()
+	car.set_dir_servo_angle(0)
+	car.forward(50)
+	time.sleep(2)
+	car.stop()
+
+	angleRange = [30,-26]
+	car.set_dir_servo_angle(angleRange[0])
+	time.sleep(0.5)
+
+	car.backward(50)
+	for i in range(angleRange[0],angleRange[1],-1):
+		car.set_dir_servo_angle(i)
+		time.sleep(.05)
+
+	car.stop()
+	car.set_dir_servo_angle(0)
 
 if __name__ == "__main__":
 	car = pcx.Picarx()
 	parkRight(car)
+	time.sleep(2)
+	parkLeft(car)
