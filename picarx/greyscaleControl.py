f6266d9be6af7fb93bfb18e2f856c1253e7c0d37
NathanJustus <nathantjustus@gmail.com>
2024-01-12 13:26:12 -0800

    Update greyscaleControl.py

--- picarx/greyscaleControl.py
+++ picarx/greyscaleControl.py
@@ -24,7 +24,7 @@ class GreyscaleController():
 if __name__ == "__main__":
     car = pc.Picarx()
     sensor = GS(car)
-    interpreter = GSI(75,'tapeDarker')
+    interpreter = GSI(80,'tapeDarker')
     controller = GreyscaleController(car,30)
     
     while(True):
