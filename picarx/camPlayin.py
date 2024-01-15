e53f6d1957bb00fc28ff869f9344cff61220337b
NathanJustus <nathantjustus@gmail.com>
2024-01-15 14:15:20 -0800

    Update camPlayin.py

--- picarx/camPlayin.py
+++ picarx/camPlayin.py
@@ -1,8 +1,6 @@
 from time import sleep
-from datetime import datetime
 from picamera import PiCamera
 from io import BytesIO
-from PIL import Image
 import cv2 as cv
 from matplotlib import pyplot as plt
 import numpy as np
@@ -18,6 +16,9 @@ for i in range(3):
 	file_bytes = np.asarray(bytearray(camStream.read()),dtype=np.uint8)
 	img = cv.imdecode(file_bytes,cv.IMREAD_COLOR)
 	camStream.seek(0)
+
+	edges = cv.Canny(img,100,200)
+
 	now = datetime.now()
 	current_time = now.strftime("%H:%M:%S")
 	print(current_time)
