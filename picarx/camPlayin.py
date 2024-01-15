c20f87d158b4c2513cf7412f023c7b0273fed902
NathanJustus <nathantjustus@gmail.com>
2024-01-15 14:16:34 -0800

    Update camPlayin.py

--- picarx/camPlayin.py
+++ picarx/camPlayin.py
@@ -3,7 +3,6 @@ from picamera import PiCamera
 from io import BytesIO
 import cv2 as cv
 from matplotlib import pyplot as plt
-import numpy as np
 
 camStream = BytesIO()
 cam = PiCamera()
@@ -18,14 +17,10 @@ for i in range(3):
 	camStream.seek(0)
 
 	edges = cv.Canny(img,100,200)
-
-	now = datetime.now()
-	current_time = now.strftime("%H:%M:%S")
-	print(current_time)
 	
 	subplotID = int('13'+str(i+1))
 	plt.subplot(subplotID)
-	plt.imshow(img,cmap='gray')
+	plt.imshow(edges,cmap='gray')
 	plt.title('Image '+str(i+1))
 
 plt.show()
